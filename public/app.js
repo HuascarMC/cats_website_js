aab9ef5fe3cc536893baa5ebfaa2cb688ea0c3ce
Huascar <huascarmc@hotmail.com>
2017-12-11 15:15:33 +0000

    created function to create new article, name and food for cat

---
+++ public/app.js
@@ -0,0 +1,18 @@
+// Add JS code to allow a new cat to be added programmatically. Do it rough and ready at first!
+var app = function() {
+
+ var article = function() {
+  var ul = document.createElement('ul');
+  ul.classList.add('.cat');
+ }
+
+ var nameIt = function(name) {
+  var catName = document.createElement('li');
+  return catName;
+ }
+
+ var feedIt = function(food) {
+  var catFood = document.createElement('li');
+  return catFood;
+ }
+}
